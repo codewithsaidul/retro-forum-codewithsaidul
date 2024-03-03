86186d3251e93fd4f87ebb351989acb6cfdcce0f
codewithsaidul <codewithsaidul@gmail.com>
2024-03-03 22:36:31 +0600

    Latest Post Section Completed

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-const loadData = async (searchId='comedy') => {
-    const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchId}`);
+const loadData = async () => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts`);
     const allPost = await res.json();
-    displayData(allPost) 
-    
+    displayData(allPost)
+
 };
 
 const displayData = (posts) => {
@@ -17,6 +17,9 @@ const displayData = (posts) => {
         const newDiv = document.createElement('div');
         newDiv.classList.add('w-full', 'border', 'border-[#797DFC]', 'rounded-3xl', 'flex', 'flex-col', 'px-5', 'gap-5', 'sm:flex-row', '[bg-[#797DFC1A]]', 'p-10')
 
+
+        const nameTitle = post.title.replace("'", "\\'");
+        // console.log(nameTitle)
 
         newDiv.innerHTML = `
             <div  class="flex justify-center sm:block indicator rounded-3xl">
@@ -33,7 +36,7 @@ const displayData = (posts) => {
                 </div>
 
                 <div class="space-y-3 border-b border-dashed pb-3 border-[#12132D40]">
-                    <h3 class="text-xl my__title text-[#12132D] font-bold title__font">${post.title}</h3>
+                    <h3 class="text-xl my__title text-[#12132D] font-bold title__font">${nameTitle}</h3>
 
                     <p class="text-base text-[#12132D99] w-full md:w-[540px]">${post.description}</p>
                 </div>
@@ -67,16 +70,16 @@ const displayData = (posts) => {
                     </div>
 
                     <button type="submit" >
-                        <img onclick="markRead(' ${post.title}', ${post.view_count})" src="./images/email.png" alt="">
+                        <img onclick="markRead(' ${nameTitle}', ${post.view_count})" src="./images/email.png" alt="">
                     </button>
                 </div>
             </div>
         `
 
-        
+
         const indeicator = newDiv.querySelector(".indicator-item");
         const status = post.isActive
-        
+
         if (status) {
             indeicator.classList.add('bg-[#10B981]');
             indeicator.classList.remove('bg-[#FF3434]');
@@ -94,17 +97,19 @@ const displayData = (posts) => {
 
 
 let count = 0;
-const myTable = document.getElementById('my-table')
+const myTable = document.getElementById('my-table');
+
+
 const markRead = (title, eye) => {
 
-    
+    // const nameTitle = title.replace()
 
     count = count + 1;
 
     const countRead = document.getElementById('count-read');
     countRead.innerText = count
-    
-   
+
+
 
     const td1 = document.createElement('td');
     const td2 = document.createElement('td');
@@ -122,9 +127,7 @@ const markRead = (title, eye) => {
     `
 
     row.appendChild(td1);
-    row.appendChild(td2)
-
-    console.log(row)
+    row.appendChild(td2);
 
     myTable.appendChild(row);
 }
@@ -135,8 +138,70 @@ const markRead = (title, eye) => {
 const handleSearch = () => {
     const searchField = document.getElementById("search-field");
     const searchText = searchField.value;
-    loadData(searchText)
-    console.log(searchText)
+    searchField.value = ''
+    // loadData(searchText)
 }
 
+// Latest Post 
+const latestPostData = async () => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/latest-posts`);
+    const allData = await res.json();
+    displayLatestPost(allData)
+}
+
+const displayLatestPost = (latests) => {
+    const latestPostContainer = document.getElementById('latest-container');
+
+    latests.forEach(item => {
+
+        let noDate = item.author.posted_date;
+
+        if (noDate === undefined) {
+            noDate = "No publish date";
+        }
+
+        let noDesg = item.author.designation
+        if (noDesg === undefined) {
+            noDesg = "Unknown";
+        }
+
+        const latestDiv = document.createElement('div');
+        latestDiv.classList.add('grid', 'grid-cols-1', 'md:grid-cols-2', 'lg:grid-cols-3', 'gap-5', 'mt-10');
+
+
+        latestDiv.innerHTML = `
+        
+        <div class="card w-full bg-[#FFFFFF] border border-[#12132D26]">
+            <figure class="px-5 pt-5">
+                <img src="${item.cover_image}" alt="Shoes"
+                class="rounded-xl" />
+            </figure>
+            <div class="card-body">
+                <h5 class="flex gap-x-3 items-center">
+                    <img src="./images/data.png" alt="">
+                    <span class="text-base text-[#12132D99] font-normal">${noDate}</span>
+                </h5>
+                <h2 class="text-lg text-[#12132D] font-bold title__font">${item.title}</h2>
+                <p class="text-base text-[#12132D99] font-normal">${item.description}</p>
+                <div class="flex gap-x-5 items-center">
+                    <div class="w-12 h-12 rounded-full">
+                        <img class="rounded-full" src="${item.profile_image}" alt="">
+                    </div>
+
+                    <div>
+                        <h3 class="text-base text-[#12132D] font-bold">${item.author.name}</h3>
+                        <p class="text-sm text-[#12132D99] font-normal mt-2">${noDesg}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        `;
+
+        latestPostContainer.appendChild(latestDiv)
+    })
+}
+
+
+latestPostData();
 loadData();
